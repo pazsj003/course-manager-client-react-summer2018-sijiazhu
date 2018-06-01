5933ccfefa595294139aa1816b1d248eb2830079
sijia zhu <sijiazhu@sijias-iMac.local>
2018-05-31 22:49:11 -0700

    add courserow

---
+++ src/components/CourseRow.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+
+class CourseRow extends React.Component {
+
+    constructor(props){
+        super(props)
+    }
+    render() {
+        return (
+            <tr><td>
+                <Link to=
+                          {`/course/${this.props.course.id}/edit`}>
+                    {this.props.course.title}
+                </Link>
+
+            </td></tr>
+        )
+    }
+}
+export default CourseRow;
