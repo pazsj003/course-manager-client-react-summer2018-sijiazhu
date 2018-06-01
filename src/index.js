a0a32f31ba31bfca8c47530eb219838a43ae1f9d
sijia zhu <sijiazhu@sijias-iMac.local>
2018-05-31 22:30:18 -0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CourseManager from './containers/CourseManager';
+ReactDOM.render(
+
+    <CourseManager/>,
+    document.getElementById('root')
+);
