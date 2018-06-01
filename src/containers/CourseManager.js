f5066b0d5cecb725e2c17ae86130ff491083fec6
sijia zhu <sijiazhu@sijias-iMac.local>
2018-05-31 22:39:14 -0700

    add courselist

--- src/containers/CourseManager.js
+++ src/containers/CourseManager.js
@@ -1,11 +1,51 @@
-import React from 'react';
+import React, {Component} from 'react'
 
+import CourseCard from '../components/CourseCard'
+import ModuleList from './ModuleList'
+import LessonTabs from './LessonTabs'
+import CourseEditor from './CourseEditor'
+import CourseList from './CourseList'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 
-class CourseManager extends React.Component {
+class CourseManger extends Component {
+
     render() {
         return (
-            <h1>Course Manager</h1>
+            <Router>
+                <div className="container-fluid">
+
+                    <h1>Course Manger</h1>
+                    {/*<CourseList/>*/}
+
+                    <Route path="/courses"
+                           component={CourseList}>
+                    </Route>
+                    <Route path="/course/:courseId/edit"
+                           component={CourseEditor}>
+                    </Route></div>
+
+                {/*<Route path="/examples"*/}
+                {/*component={CourseList}>*/}
+
+                {/*<div>*/}
+                {/*<div className="card-deck">*/}
+                {/*<CourseCard/>*/}
+                {/*<CourseCard/>*/}
+                {/*<CourseCard/>*/}
+                {/*<CourseCard/>*/}
+                {/*</div>*/}
+                {/*<CourseEditor/>*/}
+                {/*<br/>*/}
+                {/*<LessonTabs/>*/}
+                {/*<ModuleList/>*/}
+
+                {/*</div>*/}
+                {/*</Route>*/}
+                {/*</div>*/}
+            </Router>
         )
+
     }
 }
-export default CourseManager;
+
+export default CourseManger;
